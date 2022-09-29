3d05966139ff2b969c92312c3b4b94a995538340
sgm28 <sakar.michel@gmail.com>
2022-09-29 17:15:33 -0400

    Fix bug 1

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.kt
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.kt
@@ -2,6 +2,7 @@ package com.codepath.debuggingchallenges.activities
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.codepath.debuggingchallenges.R
@@ -15,12 +16,14 @@ class ChangeBackgroundActivity : AppCompatActivity() {
 
     fun onGoClick(view: View?) {
         val mainView = findViewById<View>(android.R.id.content)
-        mainView.setBackgroundColor(nextColor)
+        mainView.setBackgroundColor(Color.RED)
+        Log.i("ChangeBackground", "Started")
     }
 
     private val nextColor: Int
         private get() {
-            val newColor = if (oldColor == Color.BLUE) Color.RED else Color.BLUE
+            val newColor =
+                    if (oldColor == Color.BLUE) Color.RED else Color.BLUE
             oldColor = newColor
             return newColor
         }
--- app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.kt
@@ -8,17 +8,17 @@ import com.codepath.debuggingchallenges.R
 import java.util.*
 
 class CurrentDayActivity : AppCompatActivity() {
-    var tvDay: TextView? = null
+    private var tvDay: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current_day)
         tvDay = findViewById<View>(R.id.tvDay) as TextView
-        tvDay?.setText(dayOfMonth)
+        //The function return a Int but setText requires a string - Elijah
+        tvDay?.setText(dayOfMonth.toString())
     }
 
     private val dayOfMonth: Int
-        get() {
-            val cal = Calendar.getInstance()
-            return cal[Calendar.DAY_OF_MONTH]
-        }
-}
+    get() {
+        val cal = Calendar.getInstance()
+        return cal[Calendar.DAY_OF_MONTH]
+} }
